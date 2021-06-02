c81a2a7a4f61c1c394067424b2cc8ffe22d1b249
Seth Lawler <slawler@dewberry.com>
2021-06-02 07:01:07 -0400

    strip main for testing

--- main.go
+++ main.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/USACE/go-consequences/compute"
-	"github.com/USACE/go-consequences/consequences"
-	"github.com/USACE/go-consequences/structureprovider"
-)
-
-//Config describes the configuration settings for go-consequences.
-type Config struct {
-	SkipJWT       bool
-	LambdaContext bool
-	DBUser        string
-	DBPass        string
-	DBName        string
-	DBHost        string
-	DBSSLMode     string
-}
+import "fmt"
 
 func main() {
-	var cfg Config
-	if cfg.LambdaContext {
-		log.Print("starting server; Running On AWS LAMBDA")
-		//lambda.Start(HandleRequestArgs)
-	} else {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			params := r.URL.Query()
-			fp, fpPresent := params["FilePath"]
-			if !fpPresent {
-				http.Error(w, "No FilePath argument", http.StatusNotFound)
-			} else {
-				if len(fp[0]) == 0 {
-					//should have better error checking...
-					http.Error(w, "Invalid FilePath argument", http.StatusNotFound)
-				} else {
-					//s, _ := compute.FromFile(fp[0])
-					//fmt.Fprintf(w, s)
-					nsp := structureprovider.InitNSISP()
-					w2 := consequences.InitStreamingResultsWriter(w)
-					compute.StreamFromFileAbstract(fp[0], nsp, w2)
-				}
-			}
-		})
-		log.Print("Not on Lambda")
-		log.Print("starting local server")
-		log.Fatal(http.ListenAndServe("localhost:3030", nil))
-	}
+	fmt.Println("Hello go-consequences")
 }
